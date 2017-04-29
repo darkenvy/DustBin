06d07b4105022dab7a7463d233b043c91765bfd1
darkenvy <darkenvy6@gmail.com>
2017-04-29 15:13:35 -0700

    front-end uploads to back-end

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // app.get('/', function(req, res) {
 //   res.send('success')
 // });
+
+app.post('/upload', function(req, res) {
+  console.log(req.body.paste);
+  res.status(200).send('payload recieved')
+});
 
 app.listen(3000);
--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,32 @@
-/* globals requirejs */
-
 require.config({
   baseUrl: window.location.protocol + "//" + window.location.host
-  + window.location.pathname.split("/").slice(0, -1).join("/"),
-
+         + window.location.pathname.split("/").slice(0, -1).join("/"),
   paths: {
-    ace: "js/components/ace-builds/src"
+    ace: "js/components/ace-builds/src",
+    jquery: "js/components/jquery/dist/jquery"
   }
 });
 
-
-require(["ace/ace"], function (ace) {
+require(["jquery", "ace/ace"], function ($, ace) {
+  // Editor Setup
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/monokai");
   editor.getSession().setMode("ace/mode/javascript");
-});
 
-// requirejs(['../bower_components/ace-builds/src/ace'], function() {
-//   console.log('done');
-//   // var editor = ace.edit("editor");
-//   //     editor.setTheme("ace/theme/monokai");
-//   //     editor.getSession().setMode("ace/mode/javascript");
-// })
+  $('#upload-button').click(function() {
+    console.log(editor.getValue());
+    $.ajax({
+      method: 'POST',
+      url: '/upload',
+      data: {paste: editor.getValue()},
+      timeout: 30000,
+      success: function(data, statusCode) { // jshint ignore:line
+        console.log('data: ', data);
+      },
+      error: function(xhr, statusCode, error) {
+        console.log('error: ', error);
+      }
+    })
+  });
+
+});
